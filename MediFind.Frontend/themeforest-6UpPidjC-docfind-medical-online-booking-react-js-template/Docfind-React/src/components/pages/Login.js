6abbc3d5ab4ccb80dca757c36d8b016bbd84fe71
ConfusedPandaEvan <hwl278@nyu.edu>
2022-12-05 19:03:54 +0400

    Update Login.js

--- MediFind.Frontend/themeforest-6UpPidjC-docfind-medical-online-booking-react-js-template/Docfind-React/src/components/pages/Login.js
+++ MediFind.Frontend/themeforest-6UpPidjC-docfind-medical-online-booking-react-js-template/Docfind-React/src/components/pages/Login.js
@@ -2,6 +2,7 @@ import styles from '../../styles/Home.module.css';
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import React, { Fragment } from 'react';
+import Headertwo from '../layouts/Headertwo';
 
 
 const Login = () => {
@@ -9,6 +10,7 @@ const Login = () => {
     const [password, setPassword] = useState('')
   return (
     <Fragment>
+        <Headertwo/>
     <div className={styles.form}>
         <h1> Log In to MediFind</h1>
         <form className = {styles.myform} action ="process.php" method= "POST">

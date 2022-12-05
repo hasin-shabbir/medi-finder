e3614702ff81083c65f8f4e6cd773fb7776c52b8
B-Assali <ba2173@nyu.edu>
2022-12-05 20:41:42 +0400

    Added UserPage

--- MediFind.Frontend/themeforest-6UpPidjC-docfind-medical-online-booking-react-js-template/Docfind-React/src/components/pages/AddDrug.js
+++ MediFind.Frontend/themeforest-6UpPidjC-docfind-medical-online-booking-react-js-template/Docfind-React/src/components/pages/AddDrug.js
@@ -1,7 +1,6 @@
 
 import styles from '../../styles/Home.module.css';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import Header from "../layouts/Header";
 import React, { Fragment } from "react";
 import Breadcrumbs from '../layouts/Breadcrumbs';

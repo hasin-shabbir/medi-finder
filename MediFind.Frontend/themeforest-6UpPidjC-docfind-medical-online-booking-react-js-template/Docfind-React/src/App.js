a02a13b4fb4d5844c62eeb1dab06a9a3773e63c5
hasin-shabbir <mhs581@nyu.edu>
2022-12-09 22:41:42 +0400

    routing update

--- MediFind.Frontend/themeforest-6UpPidjC-docfind-medical-online-booking-react-js-template/Docfind-React/src/App.js
+++ MediFind.Frontend/themeforest-6UpPidjC-docfind-medical-online-booking-react-js-template/Docfind-React/src/App.js
@@ -38,7 +38,7 @@ const ScrollToTop = withRouter(({ children, location: { pathname } }) => {
 
 function App() {
   return (
-    <Router basename={"/"}>
+    <Router>
       <Suspense fallback={<div></div>}>
         <ScrollToTop>
           <Switch>

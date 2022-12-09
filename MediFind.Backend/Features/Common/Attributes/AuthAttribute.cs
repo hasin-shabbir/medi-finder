60a09720ef7e1ae9eec39a6f67d0844a4ae20f0b
hasin.shabbir <mhs581@nyu.edu>
2022-12-09 19:25:01 +0400

    Updates

--- MediFind.Backend/Features/Common/Attributes/AuthAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MediFind.Backend.Features.Common.Attributes;
-
-[AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
-public class AuthAttribute : Attribute
-{
-    public string? Claims;
-}
